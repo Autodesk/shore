c871432b5cdc585c2fe6e2ed77232fbd9c837265
eyalmor-ent <eyalmor.ent@gmail.com>
2020-10-19 19:43:39 +0300

    First commit.

---
+++ cmd/shore/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+// jsonnet "github.com/google/go-jsonnet"
+// "github.com/hashicorp/hcl/v2/hclsimple"
+// "github.com/spf13/cobra"
+)
+
+func main() {
+	println("It Works!")
+}
