52524e7542f80c8ca0de6c542e0dabfd2e63d379
slonokot <libermansa@gmail.com>
2021-02-04 10:15:11 +0200

    delete -s flag code (#65)

--- pkg/command/execute.go
+++ pkg/command/execute.go
@@ -8,7 +8,6 @@ import (
 
 // NewExecCommand - Using a Project, Renderer & Backend, executes a pipeline pipeline.
 func NewExecCommand(d *Dependencies) *cobra.Command {
-	var withSave bool
 
 	cmd := &cobra.Command{
 		Use:   "exec",
@@ -16,20 +15,6 @@ func NewExecCommand(d *Dependencies) *cobra.Command {
 		Long:  "Executes the selected pipeline",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			d.Logger.Info("Exec command invoked")
-			if withSave {
-				d.Logger.Debug("Command invoked with `save`, saving the pipeline first")
-				pipeline, err := Render(d)
-
-				if err != nil {
-					return err
-				}
-				d.Logger.Debug("Calling `Backend.SavePipeline`")
-				_, err = d.Backend.SavePipeline(pipeline)
-
-				if err != nil {
-					return err
-				}
-			}
 			d.Logger.Debug("Getting ExecArgs `Project.GetExecArgs`")
 			execArgs, err := d.Project.GetExecArgs()
 
@@ -49,8 +34,6 @@ func NewExecCommand(d *Dependencies) *cobra.Command {
 			return nil
 		},
 	}
-
-	cmd.Flags().BoolVarP(&withSave, "save", "s", false, "Render & Save the pipeline before executing it")
 
 	return cmd
 }
