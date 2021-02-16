1cb7980c3047e124a86e60fd54d05c5a6bd92bbd
Eyal Mor <>
2021-02-16 16:24:20 +0200

    Add Exec --wait option. (#70)
    1. Change the API for ExecutePipeline to return a string.
    The logic is that most pipeline runners will return an ID for a process to poll for events.
    
    2. Change the implementation of WaitForPipelineToFinish to allow for a timeout parameter.
    
    3. Add the top level configuration parameters `wait, w`, `silent, s`, `timeout, t`.
    
    Side-note, `silent` may be a good candidate for a top level paramters like `verbose, v`.

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -7,7 +7,8 @@ type Backend interface {
 	// TODO: Return type needs to be a custom wrapper in the future.
 	// We cannot assume that every backend-cli implementation will return an HTTP object.
 	SavePipeline(pipelineJSON string) (*http.Response, error)
-	ExecutePipeline(parameters string) (interface{}, *http.Response, error)
+	ExecutePipeline(parameters string) (string, *http.Response, error)
+	WaitForPipelineToFinish(id string, timeout int) (string, *http.Response, error)
 	// TODO: Reconsider `onChange`, it may be a channel to communicate data between `shore-cli` & the Testing process in an async fashion.
 	TestPipeline(testConfig string, onChange func()) error
 }
