632503ec99ba1e53fe4b1ff4263dbf8d666d7de4
dkirillov <dkirillov25@gmail.com>
2021-03-18 09:41:54 -0400

    /Issue-47: Updated shore project init (#90)
    * Work in progress - added TPLs for missing files.
    
    * Added the remaining render/e2e/exec. Passing in the ProjectName and ShortName where needed.
    
    * Moved structs down.
    
    * Fixed jsonnetfile version, updated some of the tests.
    
    * Added example_test tpl.
    
    * Cleaning up.
    
    * Minor readme change.
    
    * Update pkg/project/init.go
    
    Co-Authored-By: eyalmor-ent <eyalmor.ent@gmail.com>
    
    * Update pkg/project/init.go
    
    Co-Authored-By: eyalmor-ent <eyalmor.ent@gmail.com>
    
    * Update pkg/project/init.go
    
    Co-Authored-By: eyalmor-ent <eyalmor.ent@gmail.com>
    
    * Switched to jsonnet bundler structs.
    
    * Parent - tests - directories being created.
    
    * depName is now a URN.
    
    * Removed a bad double-quote.
    
    Co-authored-by: eyalmor-ent <eyalmor.ent@gmail.com>

--- pkg/project/init_test.go
+++ pkg/project/init_test.go
@@ -1,7 +1,6 @@
 package project_test
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/Autodeskshore/pkg/project"
@@ -111,7 +110,7 @@ func TestInitSuccess(t *testing.T) {
 		ProjectName: "my-project",
 		Renderer:    "jsonnet",
 		Backend:     "spinnaker",
-		Libraries:   []string{"github.com/Autodesk/sponnet", "github.com/***REMOVED***managed_infra"},
+		Libraries:   []string{"https://github.com/Autodeskspin-lib-jsonnet.git", "https://github.com/Autodeskadsk-lib-jsonnet.git"},
 	}
 
 	pInit := project.ProjectInitialize{
@@ -128,135 +127,24 @@ func TestInitSuccess(t *testing.T) {
 	err := pInit.Init(init)
 
 	// Assert
-	requireExists, _ := afero.Exists(localFs, "/tmp/test/require.go")
 	mainExists, _ := afero.Exists(localFs, "/tmp/test/main.pipeline.jsonnet")
+	jsonnetfileExists, _ := afero.Exists(localFs, "/tmp/test/jsonnetfile.json")
+	gitIgnoreExists, _ := afero.Exists(localFs, "/tmp/test/.gitignore")
+	readmeExists, _ := afero.Exists(localFs, "/tmp/test/README.md")
+	renderExists, _ := afero.Exists(localFs, "/tmp/test/render.yml")
+	execExists, _ := afero.Exists(localFs, "/tmp/test/exec.yml")
+	e2eExists, _ := afero.Exists(localFs, "/tmp/test/E2E.yml")
+	testExampleExists, _ := afero.Exists(localFs, "/tmp/test/tests/example_test.libsonnet")
 
-	assert.True(t, requireExists)
 	assert.True(t, mainExists)
+	assert.True(t, jsonnetfileExists)
+	assert.True(t, gitIgnoreExists)
+	assert.True(t, readmeExists)
+	assert.True(t, renderExists)
+	assert.True(t, execExists)
+	assert.True(t, e2eExists)
+	assert.True(t, testExampleExists)
 	assert.Nil(t, err)
-}
-
-type MockGoCmdInitFailure struct {
-	MockGoCmd
-}
-
-func (m *MockGoCmdInitFailure) Init(name string) (string, error) {
-	return "", errors.New("Failed due to an internal logic error")
-}
-
-func TestInitVersionInitFailure(t *testing.T) {
-	// Given
-	localFs := afero.NewMemMapFs()
-
-	init := project.ShoreProjectInit{
-		ProjectName: "my-project",
-		Renderer:    "jsonnet",
-		Backend:     "spinnaker",
-		Libraries:   []string{"github.com/Autodesk/sponnet", "github.com/***REMOVED***managed_infra"},
-	}
-
-	pInit := project.ProjectInitialize{
-		Log:   Logger,
-		GoCmd: &MockGoCmdInitFailure{},
-		Project: project.Project{
-			FS:   localFs,
-			Log:  Logger,
-			Path: "/tmp/test/",
-		},
-	}
-
-	// Test
-	err := pInit.Init(init)
-
-	// Assert
-	requireExists, _ := afero.Exists(localFs, "/tmp/test/require.go")
-	mainExists, _ := afero.Exists(localFs, "/tmp/test/main.pipeline.jsonnet")
-
-	assert.False(t, requireExists)
-	assert.False(t, mainExists)
-	assert.Error(t, err)
-}
-
-type MockGoCmdGetFailure struct {
-	MockGoCmd
-}
-
-func (m *MockGoCmdGetFailure) Get(packages []string) (string, error) {
-	return "", errors.New("Library name is wrong")
-}
-
-func TestInitVersionGetFailure(t *testing.T) {
-	// Given
-	localFs := afero.NewMemMapFs()
-
-	init := project.ShoreProjectInit{
-		ProjectName: "my-project",
-		Renderer:    "jsonnet",
-		Backend:     "spinnaker",
-		Libraries:   []string{"github.com/Autodesk/sponnet", "github.com/***REMOVED***managed_infra"},
-	}
-
-	pInit := project.ProjectInitialize{
-		Log:   Logger,
-		GoCmd: &MockGoCmdGetFailure{},
-		Project: project.Project{
-			FS:   localFs,
-			Log:  Logger,
-			Path: "/tmp/test/",
-		},
-	}
-
-	// Test
-	err := pInit.Init(init)
-
-	// Assert
-	requireExists, _ := afero.Exists(localFs, "/tmp/test/require.go")
-	mainExists, _ := afero.Exists(localFs, "/tmp/test/main.pipeline.jsonnet")
-
-	assert.True(t, requireExists)
-	assert.False(t, mainExists)
-	assert.Error(t, err)
-}
-
-type MockGoCmdVendorFailure struct {
-	MockGoCmd
-}
-
-func (m *MockGoCmdVendorFailure) Vendor() (string, error) {
-	return "", errors.New("vendor failed due to an internal logic problem")
-}
-
-func TestInitVersionVendorFailure(t *testing.T) {
-	// Given
-	localFs := afero.NewMemMapFs()
-
-	init := project.ShoreProjectInit{
-		ProjectName: "my-project",
-		Renderer:    "jsonnet",
-		Backend:     "spinnaker",
-		Libraries:   []string{"github.com/Autodesk/sponnet", "github.com/***REMOVED***managed_infra"},
-	}
-
-	pInit := project.ProjectInitialize{
-		Log:   Logger,
-		GoCmd: &MockGoCmdVendorFailure{},
-		Project: project.Project{
-			FS:   localFs,
-			Log:  Logger,
-			Path: "/tmp/test/",
-		},
-	}
-
-	// Test
-	err := pInit.Init(init)
-
-	// Assert
-	requireExists, _ := afero.Exists(localFs, "/tmp/test/require.go")
-	mainExists, _ := afero.Exists(localFs, "/tmp/test/main.pipeline.jsonnet")
-
-	assert.True(t, requireExists)
-	assert.False(t, mainExists)
-	assert.Error(t, err)
 }
 
 func TestShortName(t *testing.T) {
@@ -265,7 +153,7 @@ func TestShortName(t *testing.T) {
 		ProjectName: "my-project",
 		Renderer:    "jsonnet",
 		Backend:     "spinnaker",
-		Libraries:   []string{"github.com/Autodesk/sponnet", "github.com/***REMOVED***managed_infra"},
+		Libraries:   []string{"https://github.com/Autodeskspin-lib-jsonnet.git", "https://github.com/Autodeskadsk-lib-jsonnet.git"},
 	}
 	// Tests
 	shortName := init.ShortName()
