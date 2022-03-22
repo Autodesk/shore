288524cefa9c56411167f444d0d931d5968af56a
Daniel Kirillov <>
2022-03-22 14:57:45 -0400

    /gh-7: Loading Shore Config (#174)
    * Added config package.
    
    * Carried over LoadShoreConfig into configuration
    
    * A bit better configuration tests.
    Finding existing individual configs.
    
    * Added LoadShoreConfig file tests
    
    * default shore config is written out
    
    * added more tests
    
    * Removed getSelectorName and the associated methods/tests.
    
    * Moved to pkg/config/
    
    * Refactored LoadShoreConfig to load sooner if config is present.

---
+++ pkg/config/configuration.go
@@ -0,0 +1,151 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/Autodeskshore/pkg/command"
+	"github.com/Autodeskshore/pkg/project"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/spf13/afero"
+	"gopkg.in/yaml.v2"
+)
+
+// ShoreConfig - A structure representing the Shore Config.
+type ShoreConfig struct {
+	Renderer map[string]interface{} `json:"renderer"`
+	Executor map[string]interface{} `json:"executor"`
+	Profiles map[string]interface{} `json:"profiles"`
+}
+
+// ProfileMetaData - Metadata for a given profile.
+type ProfileMetaData struct {
+	Application string
+	Pipeline    string
+}
+
+func findIndividualLocalConfig(p *project.Project, configName string) (string, error) {
+	projectPath, err := p.GetProjectPath()
+	if err != nil {
+		return "", err
+	}
+
+	files, err := afero.ReadDir(p.FS, projectPath)
+	if err != nil {
+		return "", err
+	}
+
+	for _, f := range files {
+		baseFileName := strings.ToLower(strings.TrimSuffix(f.Name(), filepath.Ext(f.Name())))
+		if baseFileName == configName {
+			return path.Join(projectPath, f.Name()), nil
+		}
+	}
+	return "", nil
+}
+
+func writeShoreConfig(d *command.Dependencies, shoreConfig ShoreConfig) error {
+	shoreConfigBytes, err := yaml.Marshal(shoreConfig)
+	if err != nil {
+		return err
+	}
+
+	projectPath, err := d.Project.GetProjectPath()
+	if err != nil {
+		return err
+	}
+
+	shoreConfigPath := path.Join(projectPath, "shore.yml")
+	afero.WriteFile(d.Project.FS, shoreConfigPath, shoreConfigBytes, os.ModePerm)
+	d.Logger.Debugf("Created a new shore config located at: %s", shoreConfigPath)
+	return nil
+}
+
+// LoadShoreConfig - Loads the Shore Config given a Project obj.
+func LoadShoreConfig(d *command.Dependencies) (ShoreConfig, error) {
+	// TODO - change *command.Dependencies to just project.
+	var shoreConfig ShoreConfig
+
+	configData, err := command.GetConfigFileOrFlag(d, "shore", "")
+
+	if err == nil {
+		if err := jsoniter.Unmarshal(configData, &shoreConfig); err != nil {
+			// Couldn't parse the file, so error out
+			return ShoreConfig{}, err
+		}
+
+		d.Project.Log.Debug("Loaded from file Shore Config: ", shoreConfig)
+		return shoreConfig, nil
+	}
+
+	if strings.Contains(fmt.Sprint(err), "file does not exist") ||
+		strings.Contains(fmt.Sprint(err), "no such file") {
+		// Couldn't find the file, so provide a default.
+
+		homeDirName, err := os.UserHomeDir()
+		if err != nil {
+			return ShoreConfig{}, err
+		}
+
+		defaultRenderer := map[string]interface{}{
+			"type": "jsonnet",
+		}
+
+		defaultExecutor := map[string]interface{}{
+			"type": "spinnaker",
+			"config": map[string]interface{}{
+				"default": fmt.Sprintf("%s/.spin/config", homeDirName),
+			},
+		}
+
+		existingRenderPath, err := findIndividualLocalConfig(d.Project, "render")
+		if err != nil {
+			return ShoreConfig{}, err
+		} else if len(existingRenderPath) == 0 {
+			return ShoreConfig{}, fmt.Errorf(`unable to find a render config in the project`)
+		}
+
+		existingExecPath, err := findIndividualLocalConfig(d.Project, "exec")
+		if err != nil {
+			return ShoreConfig{}, err
+		} else if len(existingExecPath) == 0 {
+			return ShoreConfig{}, fmt.Errorf(`unable to find a exec config in the project`)
+		}
+
+		existingE2EPath, err := findIndividualLocalConfig(d.Project, "e2e")
+		if err != nil {
+			return ShoreConfig{}, err
+		} else if len(existingE2EPath) == 0 {
+			return ShoreConfig{}, fmt.Errorf(`unable to find a E2E config in the project`)
+		}
+
+		defaultProfiles := map[string]interface{}{
+			"default": map[string]interface{}{
+				"render": existingRenderPath,
+				"exec":   existingExecPath,
+				"e2e":    existingE2EPath,
+			},
+		}
+
+		shoreConfig = ShoreConfig{
+			Renderer: defaultRenderer,
+			Executor: defaultExecutor,
+			Profiles: defaultProfiles,
+		}
+
+		d.Project.Log.Debug("Loaded default Shore Config: ", shoreConfig)
+
+		writeShoreConfig(d, shoreConfig)
+		if err != nil {
+			return ShoreConfig{}, err
+		}
+
+		return shoreConfig, nil
+	}
+
+	// Error out on anything other than missing file.
+	return ShoreConfig{}, err
+}
---
+++ pkg/config/configuration_test.go
@@ -0,0 +1,233 @@
+package config
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/Autodeskshore/pkg/command"
+	"github.com/Autodeskshore/pkg/project"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadShoreConfigFromFile(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	testPath := "/tmp/test"
+	memFs.Mkdir(testPath, os.ModePerm)
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", testPath)
+
+	shoreConfigString := `{
+		"renderer": {
+		  "type": "jsonnet"
+		},
+		"executor": {
+		  "type": "spinnaker",
+		  "config": {
+			"default": "~/.spin/sb-config",
+			"prodSpin": "~/.spin/prod-config"
+		  }
+		},
+		"profiles": {
+		  "default": {
+			"application": "test1test2test3",
+			"pipeline": "simple-pipeline-test",
+			"render": "render.yaml",
+			"exec": "exec.yaml",
+			"e2e": "e2e.yaml"
+		  }
+		}
+	}`
+	afero.WriteFile(memFs, path.Join(testPath, "shore.json"), []byte(shoreConfigString), os.ModePerm)
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	shoreConfig, err := LoadShoreConfig(deps)
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, "test1test2test3", shoreConfig.Profiles[`default`].(map[string]interface{})[`application`])
+}
+
+func TestLoadShoreConfigFromMalformedFile(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	testPath := "/tmp/test"
+	memFs.Mkdir(testPath, os.ModePerm)
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", testPath)
+
+	shoreConfigString := `{
+		"renderer": {
+		  "type": "jsonnet"
+		},
+		"executor": {
+		  "type": "spinnaker",
+		  "config": 
+		},
+		"profiles": {
+		  "default": 
+		}
+	}`
+	afero.WriteFile(memFs, path.Join(testPath, "shore.json"), []byte(shoreConfigString), os.ModePerm)
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	shoreConfig, err := LoadShoreConfig(deps)
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.NotNil(t, err)
+	assert.Nil(t, shoreConfig.Executor)
+	assert.Nil(t, shoreConfig.Renderer)
+	assert.Nil(t, shoreConfig.Profiles)
+
+}
+
+func TestLoadShoreConfigFromIncorrectFile(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	testPath := "/tmp/test"
+	memFs.Mkdir(testPath, os.ModePerm)
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", testPath)
+
+	shoreConfigString := `{
+		"executor": "test",
+		"render": "test",
+		"profiles": "test"
+	}`
+	afero.WriteFile(memFs, path.Join(testPath, "shore.json"), []byte(shoreConfigString), os.ModePerm)
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	shoreConfig, err := LoadShoreConfig(deps)
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.NotNil(t, err)
+	assert.Nil(t, shoreConfig.Executor)
+	assert.Nil(t, shoreConfig.Renderer)
+	assert.Nil(t, shoreConfig.Profiles)
+}
+
+func TestLoadShoreConfigDefault(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	testPath := "/tmp/test"
+	memFs.Mkdir(testPath, os.ModePerm)
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", testPath)
+	afero.WriteFile(memFs, path.Join(testPath, "render.yaml"), []byte(""), os.ModePerm)
+	afero.WriteFile(memFs, path.Join(testPath, "exec.yml"), []byte(""), os.ModePerm)
+	afero.WriteFile(memFs, path.Join(testPath, "E2E.json"), []byte(""), os.ModePerm)
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	shoreConfig, err := LoadShoreConfig(deps)
+	shoreConfigExists, configErr := afero.Exists(memFs, path.Join(testPath, "shore.yml"))
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, path.Join(testPath, "render.yaml"), shoreConfig.Profiles[`default`].(map[string]interface{})[`render`])
+	assert.Equal(t, path.Join(testPath, "exec.yml"), shoreConfig.Profiles[`default`].(map[string]interface{})[`exec`])
+	assert.Equal(t, path.Join(testPath, "E2E.json"), shoreConfig.Profiles[`default`].(map[string]interface{})[`e2e`])
+	assert.Nil(t, configErr)
+	assert.True(t, shoreConfigExists)
+}
+
+func TestLoadShoreConfigMissingDefaultConfigs(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	testPath := "/tmp/test"
+	memFs.Mkdir(testPath, os.ModePerm)
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", testPath)
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	_, errRender := LoadShoreConfig(deps)
+	afero.WriteFile(memFs, path.Join(testPath, "render.yaml"), []byte(""), os.ModePerm)
+	_, errExec := LoadShoreConfig(deps)
+	afero.WriteFile(memFs, path.Join(testPath, "exec.yml"), []byte(""), os.ModePerm)
+	_, errE2E := LoadShoreConfig(deps)
+
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.NotNil(t, errRender)
+	assert.EqualError(t, errRender, `unable to find a render config in the project`)
+	assert.NotNil(t, errExec)
+	assert.EqualError(t, errExec, `unable to find a exec config in the project`)
+	assert.NotNil(t, errE2E)
+	assert.EqualError(t, errE2E, `unable to find a E2E config in the project`)
+}
+
+func TestLoadShoreConfigBadProjectPath(t *testing.T) {
+	// Given
+	memFs := afero.NewMemMapFs()
+	os.Setenv("LOCAL", "true")
+	os.Setenv("SHORE_PROJECT_PATH", "/tmp/test")
+
+	logger, _ := test.NewNullLogger()
+	deps := &command.Dependencies{
+		Project:  project.NewShoreProject(memFs, logger),
+		Renderer: nil,
+		Backend:  nil,
+		Logger:   logger,
+	}
+
+	// Test
+	_, err := LoadShoreConfig(deps)
+	os.Unsetenv("SHORE_PROJECT_PATH")
+	os.Unsetenv("LOCAL")
+
+	// Assert
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), `file does not exist`)
+}
