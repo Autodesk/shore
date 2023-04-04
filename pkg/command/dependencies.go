813c00caa5853e4e1b8303c68f9d77331fb8f09e
kirilld <daniel.kirillov@autodesk.com>
2023-04-04 15:18:43 -0400

    Added NewDependencies

--- pkg/command/dependencies.go
+++ pkg/command/dependencies.go
@@ -1,10 +1,27 @@
 package command
 
 import (
+	"fmt"
+
 	"github.com/Autodesk/shore/pkg/backend"
+	"github.com/Autodesk/shore/pkg/backend/spinnaker"
+	"github.com/Autodesk/shore/pkg/config"
 	"github.com/Autodesk/shore/pkg/project"
 	"github.com/Autodesk/shore/pkg/renderer"
+	"github.com/Autodesk/shore/pkg/renderer/jsonnet"
 	"github.com/sirupsen/logrus"
+)
+
+// Renderer Enum
+const (
+	// JSONNET - Jsonnet Renderer
+	JSONNET string = "jsonnet"
+)
+
+// Backend Enum
+const (
+	// SPINNAKER - Spinnaker Backend
+	SPINNAKER string = "spinnaker"
 )
 
 // Dependencies - Shared dependencies all controller MAY require
@@ -13,4 +30,40 @@ type Dependencies struct {
 	Backend  backend.Backend
 	Logger   logrus.FieldLogger
 	Project  *project.Project
+}
+
+// NewDependencies - Creates a Dependencies struct.
+func NewDependencies(p *project.Project) (*Dependencies, error) {
+	var chosenRenderer renderer.Renderer
+	var chosenBackend backend.Backend
+
+	shoreConfig, err := config.LoadShoreConfig(p)
+	if err != nil {
+		return &Dependencies{}, err
+	}
+
+	// Select the Renderer
+	switch shoreConfig.Renderer[`type`] {
+	case JSONNET:
+		p.Log.Debug("Using the Jsonnet Renderer")
+		chosenRenderer = jsonnet.NewRenderer(p.FS, p.Log)
+	default:
+		return &Dependencies{}, fmt.Errorf("the following Renderer is undefined: %s", shoreConfig.Renderer[`type`].(string))
+	}
+
+	// Select the Backend
+	switch shoreConfig.Executor[`type`] {
+	case SPINNAKER:
+		p.Log.Debug("Using the Spinnaker Backend")
+		chosenBackend = spinnaker.NewClient(p.Log)
+	default:
+		return &Dependencies{}, fmt.Errorf("the following Backend is undefined: %s", shoreConfig.Executor[`type`].(string))
+	}
+
+	return &Dependencies{
+		Project:  p,
+		Renderer: chosenRenderer,
+		Backend:  chosenBackend,
+		Logger:   p.Log,
+	}, nil
 }
